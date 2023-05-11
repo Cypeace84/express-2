ec5d8d3a88d6ae3b389b1274d634cb62993b51de
Cyprian Adamski <cyprian.adamski@gmail.com>
2023-05-11 08:36:17 +0200

    finish express-2

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use((req, res, next) => {
+  res.show = (name) => {
+    res.sendFile(path.join(__dirname, `/views/${name}`));
+  };
+  next();
+});
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use('/user', (req, res, next) => {
+  res.show('forbidden.html');
+});
+
+app.get('/', (req, res) => {
+  res.show('home.html');
+});
+
+app.get('/about', (req, res) => {
+  res.show('about.html');
+});
+
+app.use((req, res) => {
+  res.status(404).show('404.html');
+});
+
+app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});
